c0b733bb1f89e8847a5a0eb3b864e594ddad1d79
Bensagra <bensagra@gmail.com>
2025-07-06 17:52:06 -0300

    fix(cors): add PATCH method to CORS options

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const app = express();
 const corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
 
 };
